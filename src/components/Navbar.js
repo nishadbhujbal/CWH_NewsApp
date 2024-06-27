4a7cb6109aa510b9a8491949d11393f7083eb0d1
nishadbhujbal <nishadbhujbal128@gmail.com>
2024-06-27 12:25:17 +0530

    Fixed the hamburger icon cross btn

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,8 +14,8 @@ export default class Navbar extends Component {
     return (
       <>
         <div>
-          <nav className="bg-[#151218] flex justify-between md:justify-around text-white pt-4">
-            <div className="p-4 font-semibold">CleverBooks</div>
+          <nav className="bg-[#151218] flex justify-between items-center md:justify-around text-white">
+            <div className="p-4 font-semibold">NewsMonkey</div>
             <div>
               <button
                 id="hamburger-button"
@@ -26,32 +26,20 @@ export default class Navbar extends Component {
               </button>
             </div>
 
-            <ul className="hidden md:flex gap-4 pl-4 pt-4 text-white cursor-pointer">
+            <ul className="hidden md:flex gap-4 pl-4 text-white cursor-pointer">
               <li className="hover:bg-gradient-to-r hover:from-[#fc907e] hover:via-[#9f6eed] hover:to-[#fc907e] hover:bg-clip-text hover:text-transparent">
-                Product
-              </li>
-              <li className="hover:bg-gradient-to-r hover:from-[#fc907e] hover:via-[#9f6eed] hover:to-[#fc907e] hover:bg-clip-text hover:text-transparent">
-                Pricing
-              </li>
-              <li className="hover:bg-gradient-to-r hover:from-[#fc907e] hover:via-[#9f6eed] hover:to-[#fc907e] hover:bg-clip-text hover:text-transparent">
-                Industry
-              </li>
-              <li className="hover:bg-gradient-to-r hover:from-[#fc907e] hover:via-[#9f6eed] hover:to-[#fc907e] hover:bg-clip-text hover:text-transparent">
-                Customer Stories
+                Home
               </li>
               <li className="hover:bg-gradient-to-r hover:from-[#fc907e] hover:via-[#9f6eed] hover:to-[#fc907e] hover:bg-clip-text hover:text-transparent">
                 About
-              </li>
-              <li className="hover:bg-gradient-to-r hover:from-[#fc907e] hover:via-[#9f6eed] hover:to-[#fc907e] hover:bg-clip-text hover:text-transparent">
-                Blog
               </li>
             </ul>
             <div className="hidden md:flex items-center gap-4">
               <button className="hover:bg-gradient-to-r hover:from-[#fc907e] hover:via-[#9f6eed] hover:to-[#fc907e] hover:bg-clip-text hover:text-transparent">
                 Login
               </button>
-              <button className="bg-gradient-to-r from-pink-500 to-rose-500 text-white w-[60%] rounded-3xl p-2 font-semibold text-sm">
-                Talk to us
+              <button className="bg-gradient-to-r from-pink-500 to-rose-500 text-white w-[80%] rounded-3xl p-2 font-semibold text-sm">
+                Contact us
               </button>
             </div>
           </nav>
@@ -61,7 +49,7 @@ export default class Navbar extends Component {
           className="absolute top-0 text-3xl w-full flex-col origin-top animate-open-menu hidden"
         >
           <button
-            className="text-3xl self-end pt-4 pr-3 mb-2 text-white"
+            className="text-3xl self-end p-2 text-white"
             onClick={toggleMenu}
           >
             &times;
@@ -70,17 +58,13 @@ export default class Navbar extends Component {
             className="flex flex-col min-h-screen py-8 bg-white"
             aria-label="mobile"
           >
-            <ul className="flex flex-col gap-8 pl-4 pt-4 text-sm">
-              <li>Product</li>
-              <li>Pricing</li>
-              <li>Industry</li>
-              <li>Custmer Stories</li>
+            <ul className="flex flex-col gap-8 pl-4 text-sm">
+              <li>Home</li>
               <li>About</li>
-              <li>Blog</li>
             </ul>
             <div className="flex justify-center">
               <button className="bg-gradient-to-r from-pink-500 to-rose-500 text-white w-[30%] rounded-3xl p-2 font-semibold mt-6 text-sm">
-                Talk to us
+                Contact us
               </button>
             </div>
           </nav>
