9fd9caad8bcfa1296e1382fd378e3d737df40c8f
nishadbhujbal <nishadbhujbal128@gmail.com>
2024-06-28 19:56:47 +0530

    Hard coded the articles array with news. No API calls made!

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,33 +2,23 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <>
         <article className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-sm">
-          <img
-            alt=""
-            src="https://images.unsplash.com/photo-1600880292203-757bb62b4baf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-            className="h-56 w-full object-cover"
-          />
+          <img alt="" src={imageUrl} className="h-56 w-full object-cover" />
 
           <div className="p-4 sm:p-6">
             <a href="#">
-              <h3 className="text-lg font-medium text-gray-900">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </h3>
+              <h3 className="text-lg font-medium text-gray-900">{title}</h3>
             </a>
 
             <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Recusandae dolores, possimus pariatur animi temporibus nesciunt
-              praesentium dolore sed nulla ipsum eveniet corporis quidem,
-              mollitia itaque minus soluta, voluptates neque explicabo tempora
-              nisi culpa eius atque dignissimos. Molestias explicabo corporis
-              voluptatem?
+              {description}
             </p>
 
             <a
-              href="#"
+              href="/newsdetail/"
               className="group mt-4 inline-flex items-center gap-1 text-sm font-medium text-blue-600"
             >
               Find out more
