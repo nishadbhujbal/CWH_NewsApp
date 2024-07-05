c60f71d909d18588c7f465749edd53d63645c00d
nishadbhujbal <nishadbhujbal128@gmail.com>
2024-07-05 13:17:45 +0530

    Added the loading spinner

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
   constructor() {
@@ -12,39 +13,43 @@ export class News extends Component {
   }
 
   async componentDidMount() {
-    let url =
-      "https://newsapi.org/v2/top-headlines?country=us&apiKey=c3ccaecb001a45e0864b03adcc94ca9c&page=1&pageSize=20";
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=c3ccaecb001a45e0864b03adcc94ca9c&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: "true" });
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
+      loading: false,
     });
   }
 
   handlePrevClick = async () => {
     let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=c3ccaecb001a45e0864b03adcc94ca9c&page=${
       this.state.page - 1
-    }&pageSize=20`;
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: "true" });
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       page: this.state.page - 1,
       articles: parsedData.articles,
+      loading: false,
     });
   };
 
   handleNextClick = async () => {
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
-    } else {
+    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / 20))) {
       let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=c3ccaecb001a45e0864b03adcc94ca9c&page=${
         this.state.page + 1
-      }&pageSize=20`;
+      }&pageSize=${this.props.pageSize}`;
+      this.setState({ loading: "true" });
       let data = await fetch(url);
       let parsedData = await data.json();
       this.setState({
         page: this.state.page + 1,
         articles: parsedData.articles,
+        loading: false,
       });
     }
   };
@@ -55,25 +60,28 @@ export class News extends Component {
         <h1 className="font-semibold text-2xl text-center p-4">
           NewsMonkey - Top Headlines
         </h1>
-
+        {this.state.loading && <Spinner />}
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-8 mx-8">
-          {this.state.articles.map((element) => {
-            return (
-              <NewsItem
-                key={element.url}
-                title={element.title ? element.title : "..."}
-                description={element.description ? element.description : "..."}
-                newsUrl={element.url}
-                imageUrl={
-                  !element.urlToImage
-                    ? "https://img.apmcdn.org/7e1e37d9b02cc6a192aaa71846d6d4623a7b38d3/widescreen/b3c673-20240627-ap-flooding01-2000.jpg"
-                    : element.urlToImage
-                }
-              ></NewsItem>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <NewsItem
+                  key={element.url}
+                  title={element.title ? element.title : "..."}
+                  description={
+                    element.description ? element.description : "..."
+                  }
+                  newsUrl={element.url}
+                  imageUrl={
+                    !element.urlToImage
+                      ? "https://img.apmcdn.org/7e1e37d9b02cc6a192aaa71846d6d4623a7b38d3/widescreen/b3c673-20240627-ap-flooding01-2000.jpg"
+                      : element.urlToImage
+                  }
+                ></NewsItem>
+              );
+            })}
         </div>
-        <div className="flex justify-evenly items-center gap-4 my-4">
+        <div className="flex justify-around items-center gap-4 my-4 mx-8">
           <button
             id="prev"
             className="bg-blue-600 rounded-md p-2 text-white hover:bg-blue-500"
@@ -89,6 +97,17 @@ export class News extends Component {
             id="next"
             className="bg-blue-600 rounded-md p-2 text-white hover:bg-blue-500"
             onClick={this.handleNextClick}
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            style={{
+              opacity:
+                this.state.page + 1 >
+                Math.ceil(this.state.totalResults / this.props.pageSize)
+                  ? "0.7"
+                  : "1",
+            }}
           >
             Next &rarr;
           </button>
